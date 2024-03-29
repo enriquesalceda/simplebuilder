a2dbaae4613941da5ea4c4bdbfb018256d0d88f9
quique <enriquesalceda@gmail.com>
2024-03-29 14:40:17 +1100

    add website builder

--- src/App.js
+++ src/App.js
@@ -2,26 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 import Card from "./components/Card";
 
-function App() {
+import React from 'react';
+import {Typography, Paper, Grid} from '@material-ui/core';
+
+import { Toolbox } from '../components/Toolbox';
+import { SettingsPanel } from '../components/SettingsPanel';
+import { Topbar } from '../components/Topbar';
+
+import { Container } from '../components/user/Container';
+import { Button } from '../components/user/Button';
+import { Card } from '../components/user/Card';
+import { Text } from '../components/user/Text';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Card></Card>
-      </header>
-    </div>
+      <div style={{margin: "0 auto", width: "800px"}}>
+        <Typography variant="h5" align="center">A super simple page editor</Typography>
+        <Grid container spacing={3} style={{paddingTop: "10px"}}>
+          <Topbar />
+          <Grid item xs>
+            <Container padding={5} background="#eee">
+              <Card />
+            </Container>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper>
+              <Toolbox />
+              <SettingsPanel />
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
   );
 }
-
-export default App;
