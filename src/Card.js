8783ad344ea17c4058c5895d592312113b518e41
quique <enriquesalceda@gmail.com>
2024-03-31 17:02:26 +1100

    modify card's components text

--- src/Card.js
+++ src/Card.js
@@ -7,11 +7,11 @@ export const Card = ({background, padding = 20}) => {
     return (
         <Container background={background} padding={padding}>
             <div className="text-only">
-                <Text text="Title" fontSize={20} />
-                <Text text="Subtitle" fontSize={15} />
+                <Text text="Card Title" fontSize={20} />
+                <Text text="Card Subtitle" fontSize={15} />
             </div>
             <div className="buttons-only">
-                <Button size="small" text="Learn more" variant="contained" />
+                <Button size="small"variant="contained">Card Button</Button>
             </div>
         </Container>
     )
